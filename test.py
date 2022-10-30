a5f5b90b89665cb500a60b1fc75c16e6720dad1d
poporthailand <ismat_rr@hotmail.com>
2022-10-30 18:52:04 +0700

    Delete

--- test.py
+++
@@ -1,9 +0,0 @@
-
-import webbrowser
-import os
-
-c = webbrowser.Chrome(r'C:\Program Files\Google\Chrome\Application\chrome.exe')
-# c.open()
-file_name = os.listdir(os.path.join("Static","Save_Resume"))
-for i in file_name:
-    c.open(os.path.abspath(os.path.join("Static","Save_Resume" , i)))
